29043819be9c60dc4a42ce82bc416f677bf5505f
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-03-14 17:58:33 +0000

    automatically backed up by learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,7 +1,7 @@
 class Character < ActiveRecord::Base
   belongs_to :actor
   belongs_to :show
-  belongs_to :network, through: :show
+  belongs_to :network
 
   def say_that_thing_you_say
     self.catchprase
