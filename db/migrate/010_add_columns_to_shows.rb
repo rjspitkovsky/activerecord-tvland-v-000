b4c21e7d4bc3c327e4f0bbb21c678946c2bcff4a
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-03-14 18:14:45 +0000

    automatically backed up by learn

--- db/migrate/010_add_columns_to_shows.rb
+++ db/migrate/010_add_columns_to_shows.rb
@@ -3,6 +3,5 @@ class AddColumnsToShows < ActiveRecord::Migration[4.2]
   def change
     add_column :shows, :day, :string
     add_column :shows, :season, :string
-    add_column :shows, :network_id, :integer
   end
-end 
+end
